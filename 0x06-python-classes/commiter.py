c0c153e5ad30af054424d95ddfc3e87271cf9d40
Kidus <wizkiye@gmail.com>
2022-06-21 22:19:50 +0300

    remove unwanted files

--- 0x06-python-classes/commiter.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-files = os.listdir()
-for file in files:
-	if file != "comiter.py" and file != "main.py":
-		subprocess.run(['git', 'add', file])
-		subprocess.run(['git', 'commit', '-m', file])
