a518d91db0742609c13907f53a6feb266ddccb56
Kidus <wizkiye@gmail.com>
2022-06-21 21:06:12 +0300

    1

--- 0x05-python-exceptions/comiter.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-files = os.listdir()
-for file in files:
-	if file != "comiter.py":
-		subprocess.run(['git', 'add', file])
-		subprocess.run(['git', 'commit', '-m', file])
-
