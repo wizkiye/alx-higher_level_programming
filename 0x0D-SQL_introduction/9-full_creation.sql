10be4459a7e41fd71200d44c70495a2e414d7c11
Kidus <wizkiye@gmail.com>
2022-08-10 21:11:29 +0300

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,11 @@
--- creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows
--- creates a new table
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
--- adds new rows to the table
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
+-- script that creates a table second_table in the database hbtn_0c_0
+CREATE TABLE IF NOT EXISTS `second_table` (
+	`id` int NOT NULL,
+	`name` varchar(256),
+	`score` int
+);
+
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES ('1','John','10');
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES ('2','Alex','3');
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES ('3','Bob','14');
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES ('4','George','8');
