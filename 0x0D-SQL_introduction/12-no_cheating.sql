a835d70edeabaac0a97263880f57fbab3f7395ca
Kidus <wizkiye@gmail.com>
2022-08-10 21:11:27 +0300

    12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,2 @@
--- updates the score of Bob to 10 in the table second_table
--- updates a value in a table
-UPDATE `second_table` SET `score` = 10 WHERE `name` = 'Bob';
+-- script that updates the score of Bob to 10 in the table second_table
+UPDATE `second_table` SET score=10 WHERE name='Bob';
