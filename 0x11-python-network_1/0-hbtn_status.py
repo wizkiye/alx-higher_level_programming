d6d1d74360535a2be8eb70d40d098756699f2cf7
Kidus <wizkiye@gmail.com>
2022-09-24 00:22:10 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
 
 
 if __name__ == "__main__":
-    import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
