9df04b07f5eaa90bb3cd3adcf4cf59a79198902e
Kidus <wizkiye@gmail.com>
2022-10-26 23:17:02 +0300

    8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,4 +1,4 @@
-$.get('https://swapi.co/api/films/?format=json', function (data) {
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
   $.each(data.results, function (index, value) {
     $('<li>' + value.title + '</li>').appendTo('UL#list_movies');
   });
